fec19a596a94bee216760ffec7dbabf870f85c8c
shahnil172011 <shahnilraza4@gmail.com>
2026-07-15 18:33:33 +0530

    Upload lib/core/constant/app_strings.dart

---
+++ lib/core/constant/app_strings.dart
@@ -0,0 +1,20 @@
+class AppStrings {
+  static const appName = 'FF Sensitivity AI';
+  static const scan = 'Start Scan';
+  static const uploadHud = 'Upload HUD';
+  static const savedProfiles = 'Saved Profiles';
+  static const history = 'History';
+  static const settings = 'Settings';
+  static const about = 'About';
+  static const gyroscope = 'Gyroscope';
+  static const fingers = 'Fingers';
+  static const playStyle = 'Play Style';
+  static const fpsPreference = 'FPS Preference';
+  static const graphicsPreference = 'Graphics Preference';
+  static const general = 'General';
+  static const redDot = 'Red Dot';
+  static const scope2x = '2X Scope';
+  static const scope4x = '4X Scope';
+  static const sniperScope = 'Sniper Scope';
+  static const freeLook = 'Free Look';
+}
